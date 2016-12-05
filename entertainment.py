5427bd42a60a05398f43c778fcaa1720752eb5ce
Bryan Cash <bcash0578@gmail.com>
2016-12-05 00:58:30 -0500

    template for movie and tv_show information
    Setting up all the information for each to be filled in

--- entertainment.py
+++ entertainment.py
@@ -1,5 +1,101 @@
-# List of my favorite movies
+toy_story = media.Movie("synopsis",
+                        "rating",
+                        "duration",
+                        "poster_image",
+                        "trailer")
+# print(toy_story.storyline)
+
+rogue_one = media.Movie("synopsis",
+                        "rating",
+                        "duration",
+                        "poster_image",
+                        "trailer")
+# print(rogue_one.storyline)
+
+# rogue_one.show_trailer()
+nightmare_on_elm_street = media.Movie("synopsis",
+                                      "rating",
+                                      "duration",
+                                      "poster_image",
+                                      "trailer")
+
+jungle_book = media.Movie("The Jungle Book",
+                          "synopsis",
+                          "rating",
+                          "duration",
+                          "poster_image",
+                          "trailer")
+
+amadeus = media.Movie("Amadeus",
+                      "synopsis",
+                      "rating",
+                      "duration",
+                      "poster_image",
+                      "trailer")
+
+return_of_the_jedi = media.Movie("Star War Episode VI Return of the Jedi",
+                                 "synopsis",
+                                 "rating",
+                                 "duration",
+                                 "poster_image",
+                                 "trailer")
+
 movies = [toy_story, rogue_one, nightmare_on_elm_street, jungle_book, amadeus, return_of_the_jedi]
 
-# List of my favorite tv shows
+breaking_bad = media.Tv_show("Breaking Bad",
+                             "synopsis",
+                             "rating",
+                             "duration",
+                             "Seasons",
+                             "Episodes",
+                             "poster_image",
+                             "trailer")
+
+charmed = media.Tv_show("Charmed",
+                        "synopsis",
+                        "rating",
+                        "duration",
+                        "Seasons",
+                        "Episodes",
+                        "poster_image",
+                        "trailer")
+
+supernatural = media.Tv_show("Supernatural",
+                             "synopsis",
+                             "rating",
+                             "duration",
+                             "Seasons",
+                             "Episodes",
+                             "poster_image",
+                             "trailer")
+
+lucifer = media.Tv_show("Lucifer",
+                        "synopsis",
+                        "rating",
+                        "duration",
+                        "Seasons",
+                        "Episodes",
+                        "poster_image",
+                        "trailer")
+
+nurse_jackie = media.Tv_show("Nurse Jackie",
+                            "synopsis",
+                            "rating",
+                            "duration",
+                            "Seasons",
+                            "Episodes",
+                            "poster_image",
+                            "trailer")
+
+friends = media.Tv_show("Friends",
+                        "synopsis",
+                        "rating",
+                        "duration",
+                        "Seasons",
+                        "Episodes",
+                        "poster_image",
+                        "trailer")
+
 tvShows = [breaking_bad, charmed, supernatural, lucifer, nurse_jackie, friends]
+
+
