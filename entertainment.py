416346a530e26dab5d66787b3b647b407f72fef4
Bryan Cash <bcash0578@gmail.com>
2016-12-04 23:36:20 -0500

    entertainment.py
    file set up for my lists of movies and tv shows

---
+++ entertainment.py
@@ -0,0 +1,5 @@
+# List of my favorite movies
+movies = [toy_story, rogue_one, nightmare_on_elm_street, jungle_book, amadeus, return_of_the_jedi]
+
+# List of my favorite tv shows
+tvShows = [breaking_bad, charmed, supernatural, lucifer, nurse_jackie, friends]
