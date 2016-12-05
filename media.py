6bd30ec20f5da9524ecc0fe55f3fb8e03ea7c5c5
Bryan Cash <bcash0578@gmail.com>
2016-12-04 23:24:09 -0500

    finished classes no trailer function yet
    All classes finished, Video as a Parent Class with Movie and Tv_show
    inheriting information from Video. No trailer function yet

--- media.py
+++ media.py
@@ -1,28 +1,28 @@
 # Class for universal video information shared between movies and tv shows.
 class Video():
-    def __init__(self):
-        # title
-        # synopsis
-        # duration
-        # poster_image_url
-        # trailer_youtube_url
+    def __init__(self, title, synopsis, duration, poster_image, trailer_youtube):
+        self.title = title
+        self.synopsis = synopsis
+        self.duration = duration
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
 # Class for information used only for movies.
 class Movie(Video):
-    def __init__(self):
-        Video.__init__(self)
-        #rating
+    def __init__(self, title, synopsis, rating, duration, poster_image, trailer_youtube):
+        Video.__init__(self, title, synopsis, duration, poster_image, trailer_youtube)
+        self.rating = rating
 
     #Function that opens the browser to play trailers on youtube.
     def show_trailer(self):
 
 # Class for information used only for tv shows.
 class Tv_show(Video):
-    def __init__(self):
-        Video.__init__(self)
-        # rating
-        # seasons
-        # episodes
+    def __init__(self, title, synopsis, rating, duration, seasons, episodes, poster_image, trailer_youtube):
+        Video.__init__(self, title, synopsis, rating, duration, poster_image, trailer_youtube)
+        self.rating = rating
+        self.seasons = seasons
+        self.episodes = episodes
 
     # Function that opens the browser to play trailers on youtube.
     def show_trailer(self):
